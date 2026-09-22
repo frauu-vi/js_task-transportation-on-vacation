b9bb4e6d7f271438428b633361ac46eedf5d15d2
Viktoria Avramych <viktoria.avramych@gmail.com>
2026-09-22 13:03:53 +0300

    fixSolution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,19 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  if (days === 3) {
-    return 100;
-  }
+  const baseCost = days * 40;
+  const discountFor3To6Days = 20;
+  const additionalDiscountFor7OrMoreDays = 50;
+  const totalCostFor3To6Days = baseCost - discountFor3To6Days;
+  const totalCostFor7OrMoreDays = baseCost - additionalDiscountFor7OrMoreDays;
+  const totalCostForLessThan3Days = baseCost;
+  const totalCostFor3Days = totalCostFor3To6Days;
 
-  if (days === 6) {
-    return 220;
+  if (days >= 3 && days <= 6) {
+    return totalCostFor3To6Days;
+  } else if (days >= 7) {
+    return totalCostFor7OrMoreDays;
+  } else if (days < 3) {
+    return totalCostForLessThan3Days;
+  } else {
+    return totalCostFor3Days;
   }
-
-  if (days === 7) {
-    return 230;
-  }
-
-  return 80;
 }
-
 module.exports = calculateRentalCost;
